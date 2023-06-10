e45451be0cadd2e7a8bcabd020b74bda29bd00e7
DarkKapo <rodrigo.qc@hotmail.es>
2023-06-10 18:52:15 -0400

    enviando datos del controlador a la vista

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index","Rodrigo Quezada Candia");
         }
 
         public IActionResult Privacy()
