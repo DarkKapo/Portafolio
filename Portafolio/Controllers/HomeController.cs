e8e87fb7382c47eb53e367e97aebd7572f2b3f73
DarkKapo <rodrigo.qc@hotmail.es>
2023-06-11 19:14:12 -0400

    Se agrega servicio RepositorioProyectos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -15,46 +16,13 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
-            //Obtenermos los 3 para mostrar en el index
+            var repositorioProyectos = new RepositorioProyectos();
+            //Obtenemos los 3 para mostrar en el index
             //Tendremos otra vista donde se muestran todos
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             //Al modelo le pasamos los datos de los proyectos
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View( modelo );
-        }
-
-        private List<ProyectoDTO> ObtenerProyectos()
-        {
-            return new List<ProyectoDTO> {
-                new ProyectoDTO
-                {
-                    Titulo = "Amazon",
-                    Descripcion = "E-commerce con ASP>NET Core",
-                    ImagenURL = "/imagenes/amazon.png",
-                    Link = "https://amazon.com"
-                },
-                new ProyectoDTO
-                {
-                    Titulo = "New York Times",
-                    Descripcion = "Página de noticias en React",
-                    ImagenURL = "/imagenes/nyt.png",
-                    Link = "https://nytimes.com"
-                },
-                new ProyectoDTO
-                {
-                    Titulo = "Reddit",
-                    Descripcion = "Red social para compartir en comunidad",
-                    ImagenURL = "/imagenes/reddit.png",
-                    Link = "https://reddit.com"
-                },
-                new ProyectoDTO
-                {
-                    Titulo = "Steam",
-                    Descripcion = "Tienda en línea para comprar video juegos",
-                    ImagenURL = "/imagenes/steam.png",
-                    Link = "https://store.steampower.com"
-                }
-            };
         }
 
         public IActionResult Privacy()
---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,41 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO> {
+                new ProyectoDTO
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-commerce con ASP>NET Core",
+                    ImagenURL = "/imagenes/amazon.png",
+                    Link = "https://amazon.com"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    ImagenURL = "/imagenes/nyt.png",
+                    Link = "https://nytimes.com"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidad",
+                    ImagenURL = "/imagenes/reddit.png",
+                    Link = "https://reddit.com"
+                },
+                new ProyectoDTO
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar video juegos",
+                    ImagenURL = "/imagenes/steam.png",
+                    Link = "https://store.steampower.com"
+                }
+            };
+        }
+    }
+}
