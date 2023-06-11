790d68ce514deeff2deb6634bf61655872958bdf
DarkKapo <rodrigo.qc@hotmail.es>
2023-06-11 02:12:01 -0400

    Se agrega la lista de proyectos

---
+++ Portafolio/Models/HomeIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿//encapsula la información para cargar la vista index
+namespace Portafolio.Models
+{
+    public class HomeIndexViewModel
+    {
+        //Se guarda una enumeración de proyectos
+        public IEnumerable<ProyectoDTO> Proyectos { get; set; }
+    }
+}
---
+++ Portafolio/Models/ProyectoDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class ProyectoDTO
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string ImagenURL { get; set; }
+        public string Link { get; set; }
+    }
+}
