1e89668b4d6517d7011ec544e7a507bd43014b0d
DarkKapo <rodrigo.qc@hotmail.es>
2023-06-14 15:49:05 -0400

    se agrega el formulario con una vista de agradecimiento

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -34,6 +34,21 @@ namespace Portafolio.Controllers
             return View(proyectos);
         }
 
+        public IActionResult Contacto () { 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto( ContactoDTO contactoDTO )
+        {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias() 
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Portafolio/Models/ContactoDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Portafolio.Models
+{
+	public class ContactoDTO
+	{
+		public string Nombre { get; set; }
+		public string Email { get; set; }
+		public string Mensaje { get; set; }
+	}
+}
